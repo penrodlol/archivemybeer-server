50e9de8a3f5ec44dcf3e4643d156871d906085db
penrodlol <penrod.christian7984@gmail.com>
2020-09-09 20:35:53 -0400

    GridFS to upload images

--- src/resolvers/beer.resolver.ts
+++ src/resolvers/beer.resolver.ts
@@ -1,7 +1,11 @@
 import { Resolver, Query, Mutation, Arg } from "type-graphql";
-import { ObjectID } from "mongodb";
+import { ObjectID, MongoClient, GridFSBucket } from "mongodb";
 import { BeerPayload } from '../inputs/beer.input';
 import { Beer } from "../entity/beer.entity";
+import { GraphQLUpload } from "apollo-server-express";
+import { GraphQLScalarType } from "graphql";
+import { FileUpload } from "graphql-upload";
+import { getConnection } from "typeorm";
 
 @Resolver()
 export class BeerResolver {
@@ -36,5 +40,21 @@ export class BeerResolver {
     async delete(@Arg('id', () => String) id: string) {
         await Beer.delete({ _id: new ObjectID(id) });
         return id;
+    }
+
+    @Mutation(() => Boolean)
+    async uploadImage(@Arg('file', () => GraphQLUpload as GraphQLScalarType) file: FileUpload) {
+        const client = (getConnection().driver as any).queryRunner.databaseConnection as MongoClient;
+        const gridfs = new GridFSBucket(client.db());
+
+        return new Promise((resolve, reject) => {
+            file.createReadStream()
+                .pipe(gridfs.openUploadStream(file.filename))
+                .on('finish', () => resolve(true))
+                .on('error', (err) => {
+                    console.log(err);
+                    reject(false);
+                });
+        });
     }
 }
